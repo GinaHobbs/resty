48b24b9049405c44eb792e13d8aedac509ebee4d
GinaHobbs <ginamariehobbs@gmail.com>
2021-09-20 16:33:45 -0500

    functional components are written instead of classes

--- class-26/lab/starter-code/src/components/form/index.js
+++ class-26/lab/starter-code/src/components/form/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-
 import './form.scss';
 
-class Form extends React.Component {
+function Form(props) {
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const formData = {
       method:'GET',
       url: 'https://pokeapi.co/api/v2/pokemon',
     };
-    this.props.handleApiCall(formData);
+    props.handleApiCall(formData);
   }
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
+  return(
+    <>
+        <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
@@ -30,8 +27,7 @@ class Form extends React.Component {
           </label>
         </form>
       </>
-    );
-  }
+  )
 }
 
 export default Form;
